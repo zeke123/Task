8ee9d1beaf11d93acd9bcedec159bf06320c43d8
zhoujian <1064791247@qq.com>
2017-03-23 11:46:46 +0800

    修改bug

--- app/src/main/java/com/zhoujian/task/activity/MainActivity.java
+++ app/src/main/java/com/zhoujian/task/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.zhoujian.task.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 import com.zhoujian.task.R;
 import com.zhoujian.task.service.MyService;
 
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, MyService.class);
         startService(intent);
+
+        Toast.makeText(this, "提交代码", Toast.LENGTH_SHORT).show();
 
 
     }
