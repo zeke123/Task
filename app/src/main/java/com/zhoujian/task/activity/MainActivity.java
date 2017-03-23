2f9c7e1cedf46cd1f64506c819722fdeb52028d1
zhoujian <1064791247@qq.com>
2017-03-23 11:34:20 +0800

    第二次提交

--- app/src/main/java/com/zhoujian/task/activity/MainActivity.java
+++ app/src/main/java/com/zhoujian/task/activity/MainActivity.java
@@ -11,10 +11,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, MyService.class);
         startService(intent);
+
+
+
 
     }
 }
