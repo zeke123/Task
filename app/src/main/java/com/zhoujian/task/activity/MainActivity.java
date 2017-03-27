7b47febf6129bd9ed7da0250ff71f5c629e2f13a
zeke123 <zekezhoujian@163.com>
2017-03-27 17:21:33 +0800

    hello

--- app/src/main/java/com/zhoujian/task/activity/MainActivity.java
+++ app/src/main/java/com/zhoujian/task/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.zhoujian.task.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
 import com.zhoujian.task.R;
 import com.zhoujian.task.service.MyService;
 
@@ -16,5 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, MyService.class);
         startService(intent);
+
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
