0ae2959359179070bd292066ff6b7adfbdd020bc
zeke123 <zekezhoujian@163.com>
2017-03-27 09:22:48 +0800

    删除代码

--- app/src/main/java/com/zhoujian/task/activity/MainActivity.java
+++ app/src/main/java/com/zhoujian/task/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.zhoujian.task.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import com.zhoujian.task.R;
 import com.zhoujian.task.service.MyService;
@@ -18,11 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(MainActivity.this, MyService.class);
         startService(intent);
-        Toast.makeText(this, "提交代码", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "提交代码", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "提交代码", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "提交代码", Toast.LENGTH_SHORT).show();
-
 
     }
 }
